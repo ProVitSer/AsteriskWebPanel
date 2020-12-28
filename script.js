2ee91314ac1b9d0b851b15e72ef68e9c3ede2e6a
ProVitSer <prokinvs@gmail.com>
2020-12-28 10:45:28 +0300

    Отправка\запрос статусов LDS

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const JsonServer = require('./src/JsonServer'),
     Ari = require('./src/ari'),
+    Lds = require('./src/lds'),
     nami = require(`./models/ami`),
     WebSocket = require('ws'),
-    client = require('ari-client');
+    namiLib = require(`nami`);
+
+
 
 const ws = new WebSocket.Server({ port: 7777 });
 const jsonServer = new JsonServer();
 const ari = new Ari();
+const lds = new Lds();
 const clients = [];
+const status = {
+    "statusDND": {
+        on: '1',
+        off: ''
+    },
+    "statusHint": {
+        on: 'Busy',
+        off: 'Unavailable'
+    }
+}
 
 
 nami.on(`namiEventExtensionStatus`, (event) => {
@@ -20,13 +34,54 @@ nami.on(`namiEventExtensionStatus`, (event) => {
 
 async function transfer({ extension, transferExtension }) {
     console.log(extension, transferExtension);
-    let channelId = await ari.getExternalChannelId(transferExtension);
-    trasferCall(channelId);
+    const channelId = await ari.getExternalChannelId(transferExtension);
+    trasferCall(channelId, extension);
 }
 
 
-const trasferCall = (channelId) => {
-    console.log(channelId);
+const trasferCall = (channelId, extension) => {
+    console.log(channelId, extension);
+    let action = new namiLib.Actions.BlindTransfer();
+    action.Channel = channelId;
+    action.Context = 'from-internal-xfer';
+    action.Exten = extension;
+    nami.send(action);
+}
+const getDNDStatus = (extension) => {
+    let action = new namiLib.Actions.DbGet();
+    action.Family = 'DND';
+    action.Key = extension;
+    nami.send(action, (m) => {
+        if (m.events[0].val == '') {
+            setDNDStatus(extension, status.statusDND.on, status.statusHint.on);
+        } else {
+            setDNDStatus(extension, status.statusDND.off, status.statusHint.off);
+        }
+    });
+};
+
+const setDNDStatus = (extension, dnd, hint) => {
+    console.log(extension, status);
+    let action = new namiLib.Actions.DbPut();
+    action.Family = 'DND';
+    action.Key = extension;
+    action.Val = dnd;
+    nami.send(action, (m) => {
+        console.log(m.response);
+        if (m.response == 'Success') {
+            setHintStatus(extension, hint);
+        }
+    });
+}
+
+const setHintStatus = (extension, hint) => {
+    console.log(extension, hint);
+    let action = new namiLib.Actions.Command();
+    action.Command = `devstate change Custom:DND${extension} ${hint}`;
+    console.log(util.inspect(action));
+    nami.send(action, (m) => {
+        console.log(m);
+    });
 }
 
 const modufiExtStatus = ({ exten, status, statustext }) => {
@@ -65,12 +120,22 @@ ws.on('connection', function connection(w) {
     console.log("новое соединение " + id);
 
     w.on('message', function(message) {
+        console.log(message);
         if (message == 'get-infoList') {
             showExtStatus(w);
 
         } else {
-            let transferExt = JSON.parse(message);
-            transfer(transferExt.transfer);
+            let data = JSON.parse(message);
+            if (data.transfer) {
+                transfer(data.transfer);
+            } else if (data.dnd) {
+                getDNDStatus(data.dnd.extension);
+                console.log(data.dnd.extension);
+            } else if (data.customdnd) {
+                getDNDStatus(data.customdnd.extension);
+                console.log(data.customdnd.extension);
+            }
+
         }
 
     });
@@ -81,3 +146,41 @@ ws.on('connection', function connection(w) {
     });
 
 });
+
+
+const sendLDSStatusToAll = (event) => {
+    let ids = Object.keys(clients);
+    ids.forEach((id) => {
+        clients[id].send(JSON.stringify({ 'lds': event }));
+    });
+}
+
+
+const getLDSStatus = () => {
+    lds.getLDSStatus()
+        .then(result => {
+            showExten(result);
+        })
+}
+
+
+const showExten = (lds) => {
+    jsonServer.showExtensionStatus()
+        .then(jsonExtStatus => {
+            for (let key in jsonExtStatus) {
+                for (let n = 0; n < jsonExtStatus[key].length; n++) {
+                    for (let i = 0; i < lds.length; i++) {
+                        if (jsonExtStatus[key][n].id == lds[i]['sip_id']) {
+                            jsonServer.setLds(jsonExtStatus[key][n].id, key, lds[i]['is_active']);
+                            sendLDSStatusToAll({ id: jsonExtStatus[key][n].id, status: lds[i]['is_active'] });
+                        }
+
+                    }
+
+                }
+            }
+
+        })
+}
+
+setInterval(getLDSStatus, 60000);
